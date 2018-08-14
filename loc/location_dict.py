a5663aff76a1821d1b1fc76749b452bb88aedab1
liuzhanliang <zhanliangliu@qq.com>
2018-08-15 07:45:31 +0800

    update location_dict.py

--- loc/location_dict.py
+++ loc/location_dict.py
@@ -1,5 +1,6 @@
 import glob
 import json
+
 files = glob.glob("data/*.json")
 files.sort()
 ret = {}
@@ -24,7 +25,9 @@ def matches(v):
     while i < n:
         key, _ = t.longest_prefix(v[i:])
         if key is not None:
-            if len(v) - len(key) > 1: ret.append(key)
+            if len(v) - len(key) > 1:
+                if key != "蒙古" or (key == "蒙古" and "内蒙古" not in v):
+                    ret.append(key)
             i += len(key)
         else:
             i += 1
@@ -41,8 +44,42 @@ def long(k ,v):
         else:
             return "%s:%s" % (ret[k[:2]+"0000"], v)
 
-for k, v in ret.items():
-    m = matches(v)
-    if m:
-        print(k, v, m, long(k, v))
+def ethnicinfo():
+    for k, v in ret.items():
+        m = matches(v)
+        if m: print(k, v, m, long(k, v))
 
+province_suffix = ['省', '市', '自治区', '特别行政区']
+city_suffix = ['市', '新区', '地区', '区', '县', '自治州']
+county_suffix = ['自治县', '县', '新区', '区', '市']
+
+def names(v, suffix):
+    n = [v,]
+    m = matches(v)
+    a = v
+    for i in m: a = a.replace(i, "")
+    if a != v: n.append(a)
+    for s in suffix:
+        if a.endswith(s):
+            b = a.rstrip(s)
+            if len(b) > 1: n.append(b)
+            else: continue
+            if b == "内蒙古": n.append("内蒙")
+            if s == "自治区":
+                n.append(b+"省")
+            if s == "自治州":
+                n.append(b+"州")
+                n.append(b+"市")
+            if s == "自治县":
+                n.append(b+"县")
+            break
+    return n
+
+for k, v in ret.items():
+    if k[-4:] == '0000':
+        n = names(v, province_suffix)
+    elif k[-2:] == '00':
+        n = names(v, city_suffix)
+    else:
+        n = names(v, county_suffix)
+    print("%s\t%s\t%s" % (k, ','.join(n), ','.join(matches(v))))
